441ca52f8740f4bd2f5dd564c6e75806581e5df1
Soha <sohaali710@gmail.com>
2022-12-25 01:16:38 +0200

    initial commit

--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -69,62 +69,9 @@ export class AllProductsComponent implements OnInit {
     )
   }
 
-  //   console.log(event);
-  // if ( "cart" in localStorage){
-  //   this.cartProduct=JSON.parse(localStorage.getItem("cart")!)
-  //   let exist = this.cartProduct.find(item => item.item.id == event.item.id)
-  //   console.log(exist);
-  //   if(exist){
-  //       alert("product already added in cart")
-  //   }else {
-
-  //     this.cartProduct.push(event)
-  //     localStorage.setItem("cart",JSON.stringify(this.cartProduct))
-  //   }
-  // }else {
-  //   this.cartProduct.push(event)
-  //   localStorage.setItem("cart",JSON.stringify(this.cartProduct))
-  // }
-  // localStorage.setItem("cart",JSON.stringify(event))
-
-
-
   searchText: string = ''
   onSearchTextChanged(textValue: string) {
     this.searchText = textValue;
     console.log(this.searchText);
   }
 }
-
-
-
-
-
-
-// if ("cart" in localStorage){
-  //    console.log(this.cartProduct
-  //   );
-  //   console.log(typeof this.cartProduct
-  //     );
-
-  //   console.log(event.name)
-
-  //   this.cartProduct=JSON.parse(localStorage.getItem("cart")!)
-  //   let exist = this.cartProduct.find(item =>item.id==event.id)
-  //   if (exist){
-  //     alert("product is already in your cart")
-  //   }
-  //   else{
-  //     this.cartProduct.push(event)
-  //     localStorage.setItem( "cart",JSON.stringify(this.cartProduct))
-  //   }
-
-  // }
-  // else{
-  //    this.cartProduct.push(event)
-  //    localStorage.setItem( "cart",JSON.stringify(this.cartProduct))
-
-  // }
-
-
-// }
