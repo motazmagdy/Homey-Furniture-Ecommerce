6ebbfcede91c896fd40473f0cae3e5a1b57471b4
Soha <sohaali710@gmail.com>
2022-12-25 19:37:18 +0200

    add to cart

--- src/app/products/components/products-details/products-details.component.ts
+++ src/app/products/components/products-details/products-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../../services/products.service';
 
@@ -11,7 +11,11 @@ import { ProductsService } from '../../services/products.service';
 export class ProductsDetailsComponent implements OnInit {
   id: any;
   data: any = {};
-  constructor(private route: ActivatedRoute, private service: ProductsService) {
+
+  addButton: boolean = false;
+  amount: number = 0;
+
+  constructor(private route: ActivatedRoute, private productsService: ProductsService) {
     this.id = this.route.snapshot.paramMap.get("id")
   }
 
@@ -20,9 +24,31 @@ export class ProductsDetailsComponent implements OnInit {
   }
 
   getProduct() {
-    this.service.getProductById(this.id).subscribe(res => {
+    this.productsService.getProductById(this.id).subscribe(res => {
       this.data = res
       console.log(this.data)
     })
+  }
+
+  // add() {
+  //   this.item.emit({
+  //     //  userId:localStorage.getItem('user'),
+  //     productId: this.data._id,
+  //     quantity: this.amount
+
+  //   })
+  // }
+
+  addToCart() {
+    console.log(this.data);
+
+    let orderData={productId:this.data._id,quantity:this.amount}
+
+    this.productsService.addingToCart(orderData).subscribe(
+      {
+        next: (data) => { console.log(data) },
+        error: (err) => { console.log(err); }
+      }
+    )
   }
 }
