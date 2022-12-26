aeb6f78f9117e0a1c7576c11569a77e83877ae0c
Soha <sohaali710@gmail.com>
2022-12-26 18:57:42 +0200

    got to shopping link if cart is empty

--- src/app/carts/carts.module.ts
+++ src/app/carts/carts.module.ts
@@ -2,22 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartsComponent } from './components/carts/carts.component';
 import { CartsService } from './services/carts.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from "../shared/shared.module";
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-    declarations: [
-        CartsComponent
-    ],
-    providers: [
-        CartsService
-    ],
-    imports: [
-        CommonModule,
-        FormsModule,
-        SharedModule
-    ]
+  declarations: [
+    CartsComponent
+  ],
+  providers: [
+    CartsService
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    SharedModule
+  ]
 })
 export class CartsModule { }
