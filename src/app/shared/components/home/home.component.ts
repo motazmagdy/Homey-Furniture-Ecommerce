eaa6e078b85cf493abb3172d938a97548099addb
Soha <sohaali710@gmail.com>
2022-12-26 15:07:15 +0200

    adding spinner

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -8,44 +8,25 @@ import { HomeService } from './.././../services/home.service';
 })
 export class HomeComponent implements OnInit {
   promotedProds: any = []
-<<<<<<< HEAD
-  newPricedProd:any=[]
-  loading:boolean=false
-  constructor(private homeServ : HomeService) {
-
-     }
-=======
   newPricedProd: any = []
+  loading: boolean = false
   constructor(private homeServ: HomeService) {
 
   }
->>>>>>> b270bf5108966ce4b686e208885e37ce133ca33a
-
-
   ngOnInit(): void {
-    this.loading=true
+    this.loading = true
     this.homeServ.getPromotedProd().subscribe({
-<<<<<<< HEAD
-      next: (v) =>{
-        this.loading=false
-        this.promotedProds =v
+      next: (v) => {
+        this.loading = false
+        this.promotedProds = v
       },
 
-        error: (e) => {
-          this.loading=false
-          console.error(e)}
-      })
-=======
-      next: (v) =>
-        this.promotedProds = v,
-
-      error: (e) => console.error(e)
+      error: (e) => {
+        this.loading = false
+        console.error(e)
+      }
     })
->>>>>>> b270bf5108966ce4b686e208885e37ce133ca33a
   }
-
-
-
 
 }
 
