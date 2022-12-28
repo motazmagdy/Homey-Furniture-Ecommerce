f497dcd99f8cb2b6b3cee936204b48bf949d5de3
ayaHassan <ayagera2017@gmail.com>
2022-12-28 13:05:36 +0200

    add to wish list

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +24,27 @@ export class ProductsService {
 
   getProductById(id: any) {
     return this.http.get(environment.baseApi + 'products/' + id)
+  }
+  getWishlist(){
+   return this.http.get(environment.baseApi  + 'favorites').pipe(
+   map((result:any=[])=>{
+    console.log(result);
+  let productIds: any[] = []
+  let wishItems = result.items
+  console.log(wishItems);
+  wishItems.forEach((item: { productId: any; })=> productIds.push(item.productId))
+    return productIds;
+   })
+
+   )
+  }
+
+  addToWishlist(productId: any){
+  return this.http.post(environment.baseApi +'favorites' , {_id:productId})
+  }
+
+  removeFromWishlist(productId: any){
+    // return this.http.delete(environment.baseApi + 'favorites/' + productId)
+    return this.http.delete(environment.baseApi + `favorites/?productId=${productId}`)
   }
 }
